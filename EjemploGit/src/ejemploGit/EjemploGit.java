94a56e062184300b1b8ad3ec1cab4f2a127628ab
Rosana <Rosana@DESKTOP-LI5B5FL>
2020-04-30 10:45:50 +0200

    mensaje holamundo

---
+++ EjemploGit/src/ejemploGit/EjemploGit.java
@@ -0,0 +1,10 @@
+package ejemploGit;
+
+public class EjemploGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          System.out.println("Hola Mundo");
+	}
+
+}
