7a5fc15081f11d6d831582def1ffaa0ee42f989c
Rosana <Rosana@DESKTOP-LI5B5FL>
2020-04-30 10:50:56 +0200

    mensaje soyRosana

--- EjemploGit/src/ejemploGit/EjemploGit.java
+++ EjemploGit/src/ejemploGit/EjemploGit.java
@@ -5,6 +5,7 @@ public class EjemploGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
           System.out.println("Hola Mundo");
+          System.out.println("Soy Rosana");
 	}
 
 }
